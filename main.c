917fed66795a8e118a08a479097f7a9817e37504
bkjboadu <brightboadujnr@gmail.com>
2023-04-28 19:27:49 -0700

    i

--- main.c
+++ main.c
@@ -1,45 +1,44 @@
 #include "shell.h"
 
 /**
- * main - starting point to the project
- * @argc: arguments count
- * @argv: arguments vector
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: On success 0 otherwise 1
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	info_t detail[] = { INFO_INIT };
-	int file_desc = 2;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 			"add $3, %0"
-			: "=r" (file_desc)
-			: "r" (file_desc));
+			: "=r" (fd)
+			: "r" (fd));
 
-	if (argc == 2)
+	if (ac == 2)
 	{
-		file_desc = open(argv[1], O_RDONLY);
-		if (file_desc == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(argv[0]);
+				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(argv[1]);
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		detail->readfd = file_desc;
+		info->readfd = fd;
 	}
-	populate_env_list(detail);
-	read_history(detail);
-	hsh(detail, argv);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
-
 }
--- shell_loop.c
+++ shell_loop.c
@@ -9,37 +9,38 @@
  */
 int hsh(info_t *info, char **av)
 {
-	int status = 0;
-	ssize_t r;
-	int builtin_ret;
+	ssize_t r = 0;
+	int builtin_ret = 0;
 
-	do {
+	while (r != -1 && builtin_ret != -2)
+	{
 		clear_info(info);
 		if (interactive(info))
 			_puts("$ ");
 		_eputchar(BUF_FLUSH);
 		r = get_input(info);
-		if (r == -1)
+		if (r != -1)
 		{
-			if (interactive(info))
-				_putchar('\n');
-			break;
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
 		}
-		set_info(info, av);
-		builtin_ret = find_builtin(info);
-		if (builtin_ret == -1)
-			find_cmd(info);
+		else if (interactive(info))
+			_putchar('\n');
 		free_info(info, 0);
-	} while (builtin_ret != -2);
-
+	}
 	write_history(info);
 	free_info(info, 1);
 	if (!interactive(info) && info->status)
-		status = info->status;
-	else if (builtin_ret == -2 && info->err_num != -1)
-		status = info->err_num;
-
-	return (status);
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
 }
 
 /**
@@ -51,7 +52,6 @@ int hsh(info_t *info, char **av)
  * 1 if builtin found but not successful,
  * 2 if builtin signals exit()
  */
-
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
@@ -77,7 +77,6 @@ int find_builtin(info_t *info)
 	return (built_in_ret);
 }
 
-
 /**
  * find_cmd - finds a command in PATH
  * @info: the parameter & return info struct
@@ -87,32 +86,32 @@ int find_builtin(info_t *info)
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
-	int i, arg_count = 0;
+	int i, k;
 
 	info->path = info->argv[0];
-
-	for (i = 0; info->arg[i]; i++)
+	if (info->linecount_flag == 1)
 	{
-		if (!is_delim(info->arg[i], " \t\n"))
-			arg_count++;
+		info->line_count++;
+		info->linecount_flag = 0;
 	}
-
-	if (arg_count == 0)
+	for (i = 0, k = 0; info->arg[i]; i++)
+		if (!is_delim(info->arg[i], " \t\n"))
+			k++;
+	if (!k)
 		return;
 
 	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
-
 	if (path)
 	{
 		info->path = path;
 		fork_cmd(info);
-	} else
+	}
+	else
 	{
-		if ((interactive(info) || _getenv(info, "PATH=") || info->argv[0][0] == '/')
-				&& is_cmd(info, info->argv[0]))
-		{
+		if ((interactive(info) || _getenv(info, "PATH=")
+					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
 			fork_cmd(info);
-		} else if (*(info->arg) != '\n')
+		else if (*(info->arg) != '\n')
 		{
 			info->status = 127;
 			print_error(info, "not found\n");
@@ -120,14 +119,12 @@ void find_cmd(info_t *info)
 	}
 }
 
-
 /**
  * fork_cmd - forks a an exec thread to run cmd
  * @info: the parameter & return info struct
  *
  * Return: void
  */
-
 void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
@@ -135,6 +132,7 @@ void fork_cmd(info_t *info)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
+		/* TODO: PUT ERROR FUNCTION */
 		perror("Error:");
 		return;
 	}
